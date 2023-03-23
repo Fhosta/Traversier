7fd24bb4889c8838643f56ee3b3a284ff5b04485
florian hostachy <2032399@cegeprdl.ca>
2023-03-23 16:10:26 -0400

    Mise en place de l'ajout de client dans un fichier xml

--- traversier.py
+++ traversier.py
@@ -1,4 +1,5 @@
 import datetime
+import xml.etree.ElementTree as ET
 from PyQt5.QtCore import * 
 from PyQt5.QtGui import * 
 from PyQt5.QtWidgets import * 
@@ -18,11 +19,11 @@ class Traversier(QMainWindow):
         self.setWindowTitle("Traversier")
         self.show()
         self.listeClient = self.ui.listeClient
-        self.ui.btnClient.clicked.connect(self.obtenirResultatClient)
+        self.ui.btnClient.clicked.connect(self.ajouterClient)
 
 
 
-    def obtenirResultatClient(self):
+    def ajouterClient(self):
         nom = self.ui.nomClient.text()
         adresse = self.ui.adresseClient.text()
         ville = self.ui.villeClient.text()
@@ -30,13 +31,44 @@ class Traversier(QMainWindow):
         codePostal = self.ui.codePostalClient.text()
         telephone = self.ui.telephoneClient.text()
         courriel = self.ui.courrielClient.text()
-       
 
         unePersonne = Personne(nom, adresse, ville, province, codePostal, telephone, courriel)
 
         
+        # Enregistrement du client dans le un dossier xml 
+        client = ET.Element("client")
+        nomElement = ET.SubElement(client, "nom")
+        nomElement.text = nom
+        adresseElement = ET.SubElement(client, "adresse")
+        adresseElement.text = adresse
+        villeElement = ET.SubElement(client, "ville")
+        villeElement.text = ville
+        provinceElement = ET.SubElement(client, "province")
+        provinceElement.text = province
+        codePostalElement = ET.SubElement(client, "codePostal")
+        codePostalElement.text = codePostal
+        telephoneElement = ET.SubElement(client, "telephone")
+        telephoneElement.text = telephone
+        courrielElement = ET.SubElement(client, "courriel")
+        courrielElement.text = courriel
+        dateCreationElement = ET.SubElement(client, "dateCreation")
+        dateCreationElement.text = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        tree = ET.ElementTree(client)
+        tree.write("clients.xml", encoding="utf-8", xml_declaration=True)
+
         self.listeClient.addItem(unePersonne.nom)
 
+    def chargerClient(self):
+        root = ET.tree.getroot()
+        clients = root.findall('./client')
+        for client in clients:
+            self.model.ajouter(
+                client(client.find('nom').text, client.find('adresse').text, client.find('ville').text,
+                        client.find('province').text, client.find('codePostal').text, client.find('telephone').text,
+                        client.find('courriel').text))
+        self.model.layoutChanged.emit()
+        
 if __name__ == '__main__':
     app = QApplication([])
     window = Traversier()
